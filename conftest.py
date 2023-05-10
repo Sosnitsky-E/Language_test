ecbccc730c004c760b8cb8ee6282cd6137c9efef
Elena Sosnitsky <sosnitsky.e@gmail.com>
2023-05-10 10:19:48 +0300

    test_add_to_basket_button added

---
+++ conftest.py
@@ -0,0 +1,34 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options as OptionsFirefox
+
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default="chrome",
+                     help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action="store", default=None,
+                     help='Choose language: en/ru/fr...(etc)')
+
+
+@pytest.fixture(scope="function")
+def driver(request):
+    user_language = request.config.getoption("language")
+    browser_name = request.config.getoption("browser_name")
+    driver = None
+    if browser_name == "chrome":
+        print("\n install Chrome driver for test...")
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        driver = webdriver.Chrome(options=options)
+
+    elif browser_name == "firefox":
+        print("\n install Firefox for trest...")
+        options_firefox = OptionsFirefox()
+        options_firefox.set_preference("intl.accept_languages", user_language)
+        driver = webdriver.Firefox(options=options_firefox)
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
+    yield driver
+    print("\n quit browser")
+    driver.quit()
